ebec9fead52286984cfe405a90224c8a44a345b8
LarryChen <38965858+larrychen20011120@users.noreply.github.com>
2021-11-04 15:14:42 +0800

    Create behavior.v

---
+++ Lab1/behavior.v
@@ -0,0 +1,134 @@
+module testbench;
+
+    reg x,y,c_in;
+
+    wire s,c;
+
+    FA adder(.x(x),.y(y),.c_in(c_in),.sum(s),.c_out(c));
+
+    initial begin
+
+        x = 0;
+
+        y = 0;
+
+        c_in = 0;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 1;
+
+        y = 0;
+
+        c_in = 0;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 0;
+
+        y = 1;
+
+        c_in = 0;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 1;
+
+        y = 1;
+
+        c_in = 0;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 0;
+
+        y = 0;
+
+        c_in = 1;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 1;
+
+        y = 0;
+
+        c_in = 1;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 0;
+
+        y = 1;
+
+        c_in = 1;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        #20 
+
+        x = 1;
+
+        y = 1;
+
+        c_in = 1;
+
+        #5
+
+        $display ("X = %d , Y = %d , C_in = %d , C_out = %d , S = %d\n",x,y,c_in,c,s);
+
+        $finish;
+
+    end
+
+endmodule
+
+
+
+module FA(x,y,c_in,sum,c_out);
+
+    input x,y,c_in;
+
+    output sum,c_out;
+
+    /* modify the code here*/
+    
+    // declare reg variable to store 
+    reg sum, c_out;
+    reg sum_1, carry_1, carry_2;
+    
+    always@(*)
+    begin
+        sum_1 = x ^ y;
+        carry_1 = x & y;
+        sum = sum_1 ^ c_in;
+        carry_2 = sum_1 & c_in;
+        c_out = carry_1 + carry_2;
+    end
+
+endmodule
